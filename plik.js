92001347657f44930fc4851381049e2fae18b829
kostek.subbotko <kostek.subbotko@wp.pl>
2018-10-31 23:57:49 +0100

    aaa

--- plik.js
+++ plik.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = new express();
+const port = 3000;
 
 app.get('/', function(request, response){
     response.sendfile('index.html');
 });
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
