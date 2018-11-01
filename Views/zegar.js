cbd26834ce90f420c65fd6768f4f70e0bf2b79cd
ASUSR510J\Marcin <mo2035@wp.pl>
2018-11-01 01:48:25 +0100

    działajacy zegar

--- Views/zegar.js
+++ Views/zegar.js
@@ -3,13 +3,10 @@ var x = setInterval(function() {
   // Get todays date and time
   var now = new Date();
 
-  // Find the distance between now and the count down date
-  var distance = now;
-
   // Time calculations for days, hours, minutes and seconds
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  var hours = now.getHours();
+  var minutes = now.getMinutes();
+  var seconds = now.getSeconds();
 
   // Display the result in the element with id="demo"
   document.getElementById("zegar").innerHTML = hours + ": "
