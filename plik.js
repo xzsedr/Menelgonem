4a2b687734f3ceca2d71035f84c7ba71d7a69fe0
kostek.subbotko <kostek.subbotko@wp.pl>
2018-11-01 01:11:22 +0100

    WRESZCIE

--- plik.js
+++ plik.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = new express();
 const port = 3000;
 
+app.use(express.static('Views/'));
+
 app.get('/', function(request, response){
-    response.sendfile('index.html');
+    response.sendfile('Views/index.html');
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
